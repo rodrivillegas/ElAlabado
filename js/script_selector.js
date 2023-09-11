3b4da1d869c944c53ae2d95d210a12df52b1726a
Rodri Villegas <drvillegasrodrigo@gmail.com>
2023-09-11 04:19:31 -0300

    nueva versión

--- js/script_selector.js
+++ js/script_selector.js
@@ -1,6 +1,6 @@
 var animationContainer = document.getElementById("animation-container");
 var content = document.getElementById("content");
-var seleccionadorTragos = document.getElementById("seleccionador_tragos");
+var seleccionadorTragos = document.getElementById("opcionesBotones");
 
 // Ocultar el contenido y los botones de selección de tragos por defecto
 content.style.display = "none";
@@ -8,40 +8,25 @@ seleccionadorTragos.style.display = "none";
 
 // Esperar a que la animación termine y luego mostrar el contenido y los botones de selección de tragos
 animationContainer.addEventListener("animationend", function () {
-  animationContainer.style.display = "none";
+
   content.style.display = "block";
   seleccionadorTragos.style.display = "block";
 });
 
 // Función para mostrar los Tragos y cargar el script correspondiente
-function mostrarTragos(tipo) {
-  document.getElementById("seleccionador_tragos").style.display = "none";
+function mostrarOpciones(tipo) {
+  document.getElementById("opcionesBotones").style.display = "none";
   content.style.display = "block";
   animationContainer.style.display = "none";
 
-  if (tipo === "2x1LaMignon") {
-    cargarScript("./js/script2x1LaMignon.js");
+  if (tipo === "haceTuOrden") {
+    cargarScript("./js/haceTuOrden.js");
   }
-  if (tipo === "LaMignon") {
-    cargarScript("./js/scriptLaMignon.js");
+  if (tipo === "llamarAlMozo") {
+    cargarScript("./js/llamarAlMozo.js");
   }
-  if (tipo === "2x1Aperol") {
-    cargarScript("./js/script2x1Aperol.js");
-  } 
-  if (tipo === "2x1Mojito") {
-    cargarScript("./js/script2x1Mojito.js");
-  } 
-  if (tipo === "Aperol") {
-    cargarScript("./js/scriptAperol.js");
-  }
-  if (tipo === "FernetBotella") {
-    cargarScript("./js/scriptFernetBotella.js");
-  }
-  if (tipo === "Tutorial") {
-    cargarScript("./js/scriptTutorial.js");
-  }
-  else if (tipo === "TragosDeAutor") {
-    cargarScript("./js/scriptTragosDeAutor.js");
+  else if (tipo === "pedirLaCuenta") {
+    cargarScript("./js/pedirLaCuenta.js");
   }
 }
 
